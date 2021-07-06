5af27da7c2c87ee3771486f2415d7a628b4bd820
Dmitriy Latysh <pradovds@ukr.net>
2021-07-06 17:57:38 +0300

    fixed

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,59 @@
-import * as Buffer from "buffer";
-import {IncomingMessage, ServerResponse} from "http";
-import * as http from "http";
+import { createServer ,IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 
 const host: string = "127.0.0.1";
 const port: number = 7000;
 
-function notFound(res) {
+function notFound(res):void {
   res.statusCode = 404;
   res.setHeader("Content-Type", "text/plain");
   res.end("Not found\n");
 }
 
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   switch (req.method) {
     case "GET": {
-      try {
-        fs.readFile("input.txt", "utf-8", function (err: Error, data: Buffer):void {
+
+        fs.readFile("input.txt", "utf-8", function (err: Error, data: string):void {
           if (err) return console.error(err);
-          res.write(data.toString());
-          res.end(`Data: ${data.toString()}`)
+          res.writeHead(200, {"Content-Type": "text/plain"})
+          res.end(data)
         });
-      } catch (error) {
-         console.error(error);
-      }
 
       break;
     }
     case "POST": {
-      try {
-        let message: string = "Hello World!"
+
+        const message: string = "Hello World!"
         fs.writeFile("input.txt", message, function (err: Error): void {
           if (err) return console.error(err);
+          res.end()
         });
-      } catch (error) {
-        console.error(error);
-      }
 
       break;
     }
     case "PATCH": {
-      try {
-        let additionalData: string = "Hello, Node.js!!!"
-        fs.appendFile("input.txt", additionalData, function (err: Error): void {
-          if (err) return console.error(err);
+
+        const additionalData: string = "Hello, Node.js!!!"
+        fs.appendFile("input.txt", additionalData,{flag: "r+"}, function (err: Error): void {
+          if (err)  console.error(err);
+          res.end()
         });
-      } catch (error) {
-        console.error(error);
-      }
 
       break;
     }
     case "DELETE": {
-      try {
+
         fs.unlink("input.txt", function (err: Error) {
           if (err) return console.error(err);
-           return console.log('File "input.txt" deleted!');
+          res.end()
         });
-      } catch (error) {
-        console.error(error);
-      }
 
       break;
     }
     default: {
       notFound(res);
+
       break;
     }
   }
