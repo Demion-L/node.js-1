714bbb8aba85c0d6d82756fac1dd0d995e682d9e
Dmitriy Latysh <pradovds@ukr.net>
2021-07-06 10:57:03 +0300

    dev-branch created

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-const http = require("http"),
-  fs = require("fs");
+import * as Buffer from "buffer";
+import {IncomingMessage, ServerResponse} from "http";
+import * as http from "http";
+import * as fs from "fs";
 
-const host = "127.0.0.1";
-const port = 7000;
+const host: string = "127.0.0.1";
+const port: number = 7000;
 
 function notFound(res) {
   res.statusCode = 404;
@@ -10,11 +12,11 @@ function notFound(res) {
   res.end("Not found\n");
 }
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
   switch (req.method) {
     case "GET": {
       try {
-        fs.readFile("input.txt", function (err, data) {
+        fs.readFile("input.txt", "utf-8", function (err: Error, data: Buffer):void {
           if (err) return console.error(err);
           console.log(data.toString());
         });
@@ -25,13 +27,39 @@ const server = http.createServer((req, res) => {
       break;
     }
     case "POST": {
-       
+      try {
+        let message: string = "Hello World!"
+        fs.writeFile("input.txt", message, function (err: Error): void {
+          if (err) return console.error(err);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+
       break;
     }
     case "PATCH": {
+      try {
+        let additionalData: string = "Hello, Node.js!!!"
+        fs.appendFile("input.txt", additionalData, function (err: Error): void {
+          if (err) return console.error(err);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+
       break;
     }
     case "DELETE": {
+      try {
+        fs.unlink("input.txt", function (err: Error) {
+          if (err) return console.error(err);
+           return console.log('File "input.txt" deleted!');
+        });
+      } catch (error) {
+        console.error(error);
+      }
+
       break;
     }
     default: {
