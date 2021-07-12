3152348ef9dd5d6fba3ce0ae13cd6fd0af1d8d45
Dmitriy Latysh <pradovds@ukr.net>
2021-07-12 13:19:41 +0300

    Last fixes

--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,57 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
-import * as fs from 'fs';
-const constants = require("./constants");
+import { createServer, IncomingMessage, ServerResponse } from "http";
+import * as fs from "fs";
+import { host, port, file, encoding } from "./constants";
 
-
-function notFound(res):void {
-    res.writeHead(404, 'Not found\n', {'Content-Type': 'text/plain'});
-    res.end();
+function notFound(res: ServerResponse): void {
+  res.writeHead(404, "Not found\n", { "Content-Type": "text/plain" });
+  res.end();
 }
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   switch (req.method) {
-    case 'GET': {
-
-        fs.readFile(constants.file, constants.encoding, function (err: Error, data: string):void {
-          if (err) return console.error(err);
-          res.writeHead(200, {'Content-Type': 'text/plain'})
-          res.end(data)
-        });
+    case "GET": {
+      fs.readFile(file, encoding, function (err: Error, data: string): void {
+        if (err) return console.error(err);
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(data);
+      });
 
       break;
     }
-    case 'POST': {
+    case "POST": {
+      const message: string = "Hello World!";
+      fs.writeFile(file, message, function (err: Error): void {
+        if (err) console.error(err);
+        res.end();
+      });
 
-        const message: string = 'Hello World!'
-        fs.writeFile(constants.file, message, function (err: Error): void {
+      break;
+    }
+    case "PATCH": {
+      const additionalData: string = "Hello, Node.js!!!";
+      fs.access(file, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        fs.appendFile(file, additionalData, function (err: Error): void {
           if (err) console.error(err);
-          res.end()
+          res.end();
         });
+      });
 
       break;
     }
-    case 'PATCH': {
-
-        const additionalData: string = 'Hello, Node.js!!!'
-        fs.access(constants.file, (err) => {
-            if(err) {
-                console.error(err)
-                return
-            }
-            fs.appendFile(constants.file, additionalData, function (err: Error): void {
-                if (err) console.error(err);
-                res.end()
-            });
-        })
-
-      break;
-    }
-    case 'DELETE': {
-
-        fs.unlink(constants.file, function (err: Error) {
-          if (err) {
-              res.writeHead(404, 'File not found')
-              res.write(err)
-              res.end()
-              console.error(err);
-          }
-          res.end()
-        });
+    case "DELETE": {
+      fs.unlink(file, function (err: Error) {
+        if (err) {
+          res.writeHead(404, "File not found");
+          res.write(err);
+          res.end();
+          console.error(err);
+        }
+        res.end();
+      });
 
       break;
     }
@@ -68,7 +63,6 @@ const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   }
 });
 
-
-server.listen(constants.port, constants.host, () => {
-  console.log(`Server listens http://${constants.host}:${constants.port}`);
+server.listen(port, host, () => {
+  console.log(`Server listens http://${host}:${port}`);
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-module.exports.host = '127.0.0.1';
-module.exports.port = 7000;
-module.exports.file = 'input.txt';
-module.exports.encoding = 'utf-8';
+export const host: string = '127.0.0.1';
+export const port: number = 7000;
+export const file: string = 'input.txt';
+export const encoding: string = 'utf-8';
